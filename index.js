df33d19e7733e8ef712695fd2d0cc9c6e5d545f9
Mohamed Kaddour <kadnakamo@gmail.com>
2025-01-14 16:11:00 +0100

    add dataGhibli.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import dataManga from './data/dataManga.js'
-import cors from "cors"
+import express from "express";
+import dataManga from "./data/dataManga.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-const serverPort = 4242
+const serverPort = 4242;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/dataManga', (req, res) => {
-    res.json(dataManga)
-})
+app.get("/dataManga", (req, res) => {
+	res.json(dataManga);
+});
 
-app.get('/dataManga/:id', (req, res) => {
-    const { id } = req.params
-    const manga = dataManga.find((element) => element.id === id)
-    res.json(manga)
-})
+app.get("/dataManga/:id", (req, res) => {
+	const { id } = req.params;
+	const manga = dataManga.find((element) => element.id === id);
+	res.json(manga);
+});
+
+app.get("/dataGhibli", (req, res) => {
+	res.json(dataGhibli);
+});
 
 app.listen(serverPort, () => {
-    console.info("l'api est lancée")
-})
+	console.info("l'api est lancée");
+});
 
-export default app
+export default app;
