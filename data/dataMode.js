b69f9f21a509631fed8e4f55f756f3535c00d820
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-01-14 16:11:19 +0100

    First commit

---
+++ data/dataMode.js
@@ -0,0 +1,26 @@
+const dataMode = [
+	{
+		id: "1",
+		image: "",
+		aetiste_name: "One",
+		name_work: "Ampli",
+		date: "Printemps 2016, Brighton",
+		format: "portrait",
+		description: "",
+		size_frame: "70cm x 50cm",
+		price: "AskMe",
+	},
+
+	{
+		id: "2",
+		image: "",
+		artist_name: "two",
+		name_work: "Nina Alvar",
+		date: "Printemps 2017, Washington",
+		format: "portrait",
+		description: "",
+		size_frame: "70cm x 50cm",
+		price: "AskMe",
+	},
+];
+export default dataMode;
