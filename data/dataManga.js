41a288de0d78268a1440ae55528744b7af144307
luciemiszewski <miszewskilucie@outlook.com>
2025-01-17 10:37:45 +0100

    String

--- data/dataManga.js
+++ data/dataManga.js
@@ -1,36 +1,36 @@
 const dataManga = [
     {
-        id: "1",
+        id: 1,
         nameWork: "Moriarty The Patriot",
         artistName: "Ryōsuke Takeuchi",
         image: "https://i.pinimg.com/736x/86/93/74/86937422ab37f2d96df91641943f068e.jpg"
     },
     {
-        id: "2",
+        id: 2,
         nameWork: "Bungou Stray Dogs",
         artistName: "Kafka Asagiri",
         image: "https://i.pinimg.com/736x/c4/df/70/c4df70937f7fd022685f20918bd27c71.jpg"
     },
     {
-        id: "3",
+        id: 3,
         nameWork: "Death Note",
         artistName: "Tsugumi Ohba",
-        image: "https://example.com/death-note.jpg"
+        image: "https://i.pinimg.com/736x/0f/11/5d/0f115d7be37c1b13f727849a21ea33d7.jpg"
     },
     {
-        id: "4",
+        id: 4,
         nameWork: "Dragon Ball",
         artistName: "Akira Toriyama",
         image: "https://example.com/attack-on-titan.jpg"
     },
     {
-        id: "5",
+        id: 5,
         nameWork: "Jujustu Kaisen",
         artistName: "Gege Akutami",
         image: "https://example.com/one-piece.jpg"
     },
     {
-        id: "6",
+        id: 6,
         nameWork: "Demon Slayer",
         artistName: "Koyoharu Gotōge",
         image: "https://example.com/one-piece.jpg"
