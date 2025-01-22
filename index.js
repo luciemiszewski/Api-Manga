c2f8c2b112562b0740710887da313a2e97b47e0b
Thurzas <mathieu.miot@gmail.com>
2025-01-22 10:19:20 +0100

    fixing cors issues

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import dataDnD from "./data/dataDnD.js";
 import dataSport from "./data/dataSport.js";
 const app = express();
 const corsOptions = {
-	origin:"http://localhost:3000/",
+	origin:"http://localhost:3000",
 	allowedHeaders:["Content-Type  Authorization"],
 	methods:["GET"]
 };
